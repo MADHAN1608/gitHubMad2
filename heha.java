6945a149636c21942cfdab3d0d678f6e2ec09f6b
MADHAN1608 <r.madhankumar1608@gmail.com>
2026-09-02 19:31:17 +0530

    created heha.java

---
+++ heha.java
@@ -0,0 +1,15 @@
+class A {
+    int a = 10;
+    void show () {
+        System.out.println(a);
+    }
+}
+
+
+public class heha {
+    public static void main(String[] args) {
+        A hi = new A();
+        int g = hi.a;
+        System.out.println(g);
+    }
+}
